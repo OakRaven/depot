e829821851382285c988ddbc1ca48763741cdf00
Mike Hatfield <oakraven13@gmail.com>
2012-08-30 20:36:49 -0300

    Completed chapter 9.

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,12 +1,17 @@
 class StoreController < ApplicationController
   def index
     @products = Product.find_products_for_sale
+    @cart = find_cart
   end
 
   def add_to_cart
   	product = Product.find(params[:id])
   	@cart = find_cart
-  	@cart.add_product(product)
+  	@current_item = @cart.add_product(product)
+    respond_to do |format|
+      format.js if request.xhr?
+      format.html { redirect_to_index }
+    end
   rescue ActiveRecord::RecordNotFound
   	logger.error("Attempt to access invalid product #{params[:id]}")
   	redirect_to_index('Invalid Product')
@@ -14,11 +19,11 @@ class StoreController < ApplicationController
 
   def empty_cart
     session[:cart] = nil
-    redirect_to_index('Your cart has been emptied')
+    redirect_to_index
   end
 
-  def redirect_to_index(message)
-    flash[:notice] = message
+  def redirect_to_index(message = nil)
+    flash[:notice] = message if message
     redirect_to :action => 'index'
   end
 
